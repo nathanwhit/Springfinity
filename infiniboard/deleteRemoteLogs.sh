9ee7a32edf1d564dd76fe7aaef1fab1b37a2c896
nathanwhit <nw7ct@virginia.edu>
2019-05-01 21:42:30 -0400

    Convenience script

---
+++ infiniboard/deleteRemoteLogs.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/zsh
+remote_log_dir='/var/mobile/Documents/InspectiveC/SpringBoard'
+local_log_dir='./logs'
+ssh '-T' '-p' "${THEOS_DEVICE_PORT}" "root@${THEOS_DEVICE_IP}" << EOF
+    find "${remote_log_dir}" -name '*.log' -delete
+    exit
+EOF
+
+echo "All logs deleted"
