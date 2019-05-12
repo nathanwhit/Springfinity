d478cc6a98833b79266464225e39adf3a6f82432
nathanwhit <nwhit131@gmail.com>
2019-05-12 00:00:24 -0400

    Rename/rebrand, fix paging not updating properly

--- infiniboard/infiniboard12prefs/IFBRootListController.h
+++
@@ -1,8 +0,0 @@
-#import <Preferences/PSListController.h>
-#import <Preferences/PSSpecifier.h>
-
-@interface IFBRootListController : PSListController
-- (id)readPreferenceValue:(PSSpecifier*)specifier;
-- (NSArray *)specifiers;
-- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier;
-@end
